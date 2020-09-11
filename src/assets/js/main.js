99495d3cdc3796c98f24bbaa3db75390f86ab358
Leonid Shurakov <7noptic@gmail.com>
2020-09-11 15:33:39 +0300

    version 1.1 -adaptive 1 slide, - animation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,18 +2,28 @@ let slide1Right = document.querySelectorAll('.pill-selection'),
     slide1Left = document.querySelectorAll('.pet-selection > a, .pet-selection>h3,nav,.header__logo'),
     slide1Fade = document.querySelector('.header');
 
+    slide2Fade = document.querySelector('.card'),
+
     sections = document.querySelectorAll('section'),
 
-    link = document.querySelector('.pet-selection__link');
+    link = document.querySelector('.pet-selection__link'),
+
+    card = document.querySelector('.card'),
+        cardLink = card.querySelectorAll('a');
 
 link.addEventListener('click', function () {
-    itemRightOut(slide1Right);
+    itemsRightOut(slide1Right);
     itemLeftOut(slide1Left);
     fadeOut(slide1Fade);
-    setTimeout(displayNone, 750, sections[0]);
-    setTimeout(displayFlex, 750, sections[1]);
-
+    setTimeout(displayNone, 650, sections[0]);
+    setTimeout(displayFlex, 650, sections[1]);
+    fadeIn(sections[1]);
 });
+
+card.addEventListener('click', function() {
+    if (!event.target('a')) return
+    alert('asdfsa');
+})
 
 function displayNone(item) {
     item.style.display = 'none';
@@ -23,16 +33,21 @@ function displayFlex(item) {
     item.style.display = 'flex';
 }
 
-function itemRightOut(item) {
-    for (let key of item) {
-        key.classList.add('transition', 'item__right-out');
-    }
+function itemsRightOut(item) {
+        for (let key of item) {
+            key.classList.add('transition', 'item__right-out');
+        }
 }
+
 
 function itemLeftOut(item) {
     for (let key of item) {
         key.classList.add('transition', 'item__left-out');
     }
+}
+
+function itemBottomIn(item) {
+    item.classList.add('transition', 'item__bottom-in');
 }
 
 function itemTopOut(item) {
@@ -45,4 +60,7 @@ function itemBottomOut(item) {
 
 function fadeOut(item) {
     item.classList.add('transition', 'item__fade-out');
+}
+function fadeIn(item) {
+    item.classList.add('item__fade-in');
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './assets/styles/styles.scss'
+import './assets/styles/grid.scss'
 import './assets/styles/animation.scss'
+import './assets/styles/media.scss'
 import './assets/js/main'
